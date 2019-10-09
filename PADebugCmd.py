5415ca1c096d8e61b31ecd222c0a73c5d639ff46
nikonau <51310129+nikonau@users.noreply.github.com>
2019-10-09 21:44:22 +0800

    Create PADebugCmd.py

---
+++ PADebugCmd.py
@@ -0,0 +1,22 @@
+import paramiko
+import time
+
+USERNAME = 'username'
+PASSWORD = 'password'
+HOSTNAME = '192.168.0.1'  #Firewalls IP
+PORT = 22
+
+def ssh_command(username=USERNAME, password=PASSWORD, hostname=HOSTNAME, port=PORT):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.load_system_host_keys()
+    ssh_client.connect(hostname, port, username, password)
+    remote_conn = ssh_client.invoke_shell()
+    #print ("Interactive SSH session established")
+    remote_conn.send("debug software restart process dnsproxy\n ")
+    time.sleep(8)
+    remote_conn.send("exit\n ")
+
+
+if __name__ == '__main__':
+    ssh_command()
